0a44f576495a542a6a1e87598117029c534f611a
d0422 <rlfehd2013@naver.com>
2024-02-07 13:48:16 +0900

    config: 프로젝트 설정
    - git commit message 컨벤션 적용
    - styled-components추가
    - nextjs 프로젝트 구성
    - README commit 컨벤션 작성
    - README Task 분리 및 체크포인트 기재

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,72 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {' '}
+            {initialProps.styles} {sheet.getStyleElement()}{' '}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href={`https://fonts.googleapis.com/css2?family=Inter:wght@400;900;500;600;700;800&display=swap`}
+            rel="stylesheet"
+          />
+          <link
+            href={`https://webfontworld.github.io/gmarket/GmarketSans.css`}
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href={`https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css`}
+          />
+          <link
+            href={`https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap`}
+            rel="stylesheet"
+          />
+          <script
+            src={`https://apis.google.com/js/platform.js?onload=init`}
+            async
+            defer
+          ></script>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="icons/icon-192x192.png"></link>
+          <meta name="msapplication-TileColor" content="#FFFFFF"></meta>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
