b3dfa4971513b9aa65ab2c11cb4bea71d6625a89
d0422 <rlfehd2013@naver.com>
2024-02-08 00:10:48 +0900

    test: useInput 테스트 추가
    - useInput 초기값 테스트
    - useInput additionalChange함수 테스트

---
+++ src/hooks/__tests__/useInput.test.ts
@@ -0,0 +1,25 @@
+import { renderHook } from '@testing-library/react';
+import useInput from '../useInput';
+import React from 'react';
+
+describe('useInput기능 테스트', () => {
+  it('useInput의 초기값을 설정할 수 있다.', () => {
+    const testFn = jest.fn();
+    const initialInput = 'TEST';
+    const { result } = renderHook(() => useInput(initialInput, testFn));
+    expect(result.current.value).toStrictEqual('TEST');
+  });
+
+  it('useInput에 additionalChange로 주입시킨 함수를 event.target.value를 인자로 하여 수행시킬 수 있다.', () => {
+    const testFn = jest.fn();
+
+    const event = {
+      target: { value: '1' },
+    } as React.ChangeEvent<HTMLInputElement>;
+
+    const { result } = renderHook(() => useInput('', testFn));
+    result.current.handleChange(event);
+
+    expect(testFn).toHaveBeenCalledWith('1');
+  });
+});
