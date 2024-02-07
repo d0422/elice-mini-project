2aaa1889d58a7c25aa81c1dd216de6ff2bb7c656
d0422 <rlfehd2013@naver.com>
2024-02-07 22:42:42 +0900

    feat: chips에 대한 or(chips의 params)를 여러개 받을 수 있도록 처리
    chips에 대한 or(chips의 params)를 여러개 받을 수 있도록 처리

--- src/api/getCourse.ts
+++ src/api/getCourse.ts
@@ -2,23 +2,25 @@ import { PAGE } from '@/constants/PAGE';
 import { OrgCourseListResponses } from '@/types/OrgCourseListResponse';
 import axios from 'axios';
 
+export interface ChipsParams {
+  is_free: boolean;
+  enroll_type: 0 | 4;
+}
 export interface GetCourseParams {
   title: string;
-  is_free?: boolean;
-  enroll_type?: 0 | 4;
+  chips: ChipsParams[];
   offset?: number;
 }
 
 export default async function getCourse({
   title,
-  is_free,
-  enroll_type,
+  chips,
   offset,
 }: GetCourseParams) {
   const result = await axios.get<OrgCourseListResponses>('/api/getCourse', {
     params: {
       filter_conditions: JSON.stringify({
-        $and: [{ title: `%${title}%` }, { $or: [{ enroll_type, is_free }] }],
+        $and: [{ title: `%${title}%` }, { $or: [...chips] }],
       }),
       offset: offset || 0,
       count: PAGE.PAGE_EACH_ELEMENT_COUNT,
